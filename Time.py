1206bae15681b3a7bbd7b878adf6e027673621a9
Sajawe <sajawesoso@gmail.com>
2024-09-10 14:49:56 +0200

    Delete Time.py

--- Time.py
+++
@@ -1,7 +0,0 @@
-import datetime
-TodayDate = datetime.datetime.now()
-Hours = TodayDate.strftime("%H")
-Minutes = TodayDate.strftime("%m")
-Seconds = TodayDate.strftime("%S")
-Formatted = Hours +" hours, " + Minutes +" minutes and " + Seconds +" seconds"
-print(Formatted)
